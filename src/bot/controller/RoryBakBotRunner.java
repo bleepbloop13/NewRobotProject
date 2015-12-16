da9f38f893165a560c5db69305eb3105b06d7e4b
bleepbloop13 <rbak9218@740s-pl-161.local>
2015-12-16 12:09:43 -0700

    Runner and Controller

---
+++ src/bot/controller/RoryBakBotRunner.java
@@ -0,0 +1,12 @@
+package bot.controller;
+
+public class RoryBakBotRunner
+{
+
+	public static void main(String[] args)
+	{
+		RobotController myRobotController = new RobotController();
+		myRobotController.start();
+	}
+
+}
