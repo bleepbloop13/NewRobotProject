b3a06df2bfefb10e82351a60f6f312703cb4b01e
bleepbloop13 <rbak9218@740s-pl-161.local>
2016-01-05 13:55:29 -0700

    Cleaned Controller and added code to EV3Bot

--- src/bot/controller/RobotController.java
+++ src/bot/controller/RobotController.java
@@ -1,14 +1,18 @@
 package bot.controller;
 
+import bot.model.EV3Bot;
+
 public class RobotController
 {
+	private EV3Bot roryBot;
+	
 	public RobotController()
 	{
-		
+		roryBot = new EV3Bot();
 	}
 
 	public void start()
 	{
-		
+		roryBot.driveRoom();
 	}
 }
--- src/bot/model/EV3Bot.java
+++ src/bot/model/EV3Bot.java
@@ -1,6 +1,13 @@
 package bot.model;
 
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
+
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.navigation.MovePilot;
+
 import lejos.utility.Delay;
 
 public class EV3Bot
@@ -9,34 +16,43 @@ public class EV3Bot
 	private int xPosition;
 	private int yPosition;
 	private long waitTime;
-	
+
+	private MovePilot botPilot;
+
 	public EV3Bot()
 	{
 		this.message = "Rory codes roryBot";
 		this.xPosition = 50;
 		this.yPosition = 50;
 		this.waitTime = 4000;
-		
-		displayMessage();
+
+		setupPilot();
 	}
-	
-	
+
+	private void setupPilot()
+	{
+		Wheel leftWheel = WheeledChassis.modelWheel(Motor.A, 43.2).offset(-72);
+		Wheel rightWheel = WheeledChassis.modelWheel(Motor.B, 43.2).offset(72);
+		Chassis baseChassis = new WheeledChassis(new Wheel[] { leftWheel, rightWheel }, WheeledChassis.TYPE_DIFFERENTIAL);
+		botPilot = new MovePilot(baseChassis);
+	}
+
 	public void driveRoom()
 	{
-		//call private helper here
-		displayMessage("driveRoom");
+		botPilot.travel(254.12);
 	}
-	
+
 	private void displayMessage()
 	{
 		LCD.drawString(message, xPosition, yPosition);
 		Delay.msDelay(waitTime);
+		LCD.clear();
 	}
-	
+
 	private void displayMessage(String message)
 	{
 		LCD.drawString(message, xPosition, yPosition);
 		Delay.msDelay(waitTime);
 	}
-	
+
 }
